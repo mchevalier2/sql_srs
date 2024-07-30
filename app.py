9a5cd2f7725cf2e83c09e54445aefaed60fcf7b5
Manu Chevalier <it@manuelchevalier.com>
2024-07-30 08:58:13 +0200

    catching up with main

--- app.py
+++ app.py
@@ -16,16 +16,15 @@ option = st.selectbox(
 
 st.write("You select:", option)
 
-
 data = {'a': [1, 2, 3], 'b': [4, 5, 6]}
 df = pd.DataFrame(data)
 
 tab1, tab2, tab3 = st.tabs(["Cat", "Dog", "Owl"])
 with tab1:
-        sql_query = st.text_area("label text")
-        st.write(f"Vous avez rentré la requete: {sql_query}")
-        result = duckdb.query(sql_query).df()
-        st.dataframe(result)
+    sql_query = st.text_area("label text")
+    st.write(f"Vous avez rentré la requete: {sql_query}")
+    result = duckdb.query(sql_query).df()
+    st.dataframe(result)
 
 with tab2:
     st.header("A dog")
